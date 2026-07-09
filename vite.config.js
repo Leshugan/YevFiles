6b3332aa83b920f385329bf01e7b79b0cf5d42d4
Yevhenii <Leshugan@gmail.com>
2026-07-09 11:31:37 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base: "./" обязателен — Capacitor грузит файлы по file://
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-});
